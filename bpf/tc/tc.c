575da05fc314a6989c97a374e2c2b1ae229f889a
fchrgrib <13521031@mahasiswa.itb.ac.id>
2025-05-17 03:43:38 +0700

    feat: load bpf maps

--- bpf/tc/tc.c
+++ bpf/tc/tc.c
@@ -59,16 +59,16 @@ int main(int argc, char **argv)
                 fprintf(stderr, "Nodeport value must be in range <30000, 32000>\n");
                 return 1;
         }
-		int svc_pod_ips = bpf_object_get("/sys/fs/bpf/svc_pod_ips");
+		int svc_pod_ips = bpf_obj_get("/sys/fs/bpf/svc_pod_ips");
 		if (svc_pod_ips < 0) {
-			DEBUG_BPF_PRINTK("bpf_obj_get() failed\n")
+			fprintf("bpf_obj_get() failed\n")
 			return TC_ACT_OK;
 		}
-		DEBUG_BPF_PRINTK("bpf_obj_get() returned fd %d\n", svc_pod_ips)
+		fprintf("bpf_obj_get() returned fd %d\n", svc_pod_ips)
 
-		int hash_map = bpf_object_get("/sys/fs/bpf/hash_map");
+		int hash_map = bpf_obj_get("/sys/fs/bpf/hash_map");
 		if (hash_map < 0) {
-			DEBUG_BPF_PRINTK("bpf_obj_get() failed\n")
+			fprintf("bpf_obj_get() failed\n")
 			return TC_ACT_OK;
 		}
 
