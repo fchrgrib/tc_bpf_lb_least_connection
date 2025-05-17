ce6eb046e78cfd924f619698c7c12607c04c338b
fchrgrib <13521031@mahasiswa.itb.ac.id>
2025-05-18 06:38:36 +0700

    feat: add pin path and iterate least connection ip

--- bpf/tc/tc.c
+++ bpf/tc/tc.c
@@ -161,8 +161,8 @@ int main(int argc, char **argv)
 		__u32 selected_ip = 0;
 
 		while (bpf_map_get_next_key(svc_pod_ips, &key_ip, &next_key) == 0) {
-			if (bpf_map_lookup_elem(svc_pod_ips, &next_key, &value_ip) == 0) {
-				if (bpf_map_lookup_elem(hash_map, &value_ip.ip_address, &value) == 0) {
+			if (bpf_map_lookup_elem(svc_pod_ips, &next_key, &value_ip) != 0) {
+				if (bpf_map_lookup_elem(hash_map, &value_ip.ip_address, &value) != 0) {
 					if (value < min_conn) {
 						min_conn = value;
 						selected_ip = value_ip.ip_address;
